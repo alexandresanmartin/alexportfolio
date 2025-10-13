5c89296c63ea5dc036f7b95085bd3aecb1dde058
ALEX <alex@example.com>
2025-10-13 17:43:19 +0200

    Update base URL for alexportfolio repository

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/mech-cali-showcase-64034/',
+  base: '/alexportfolio/',
   server: {
     host: "::",
     port: 3001,
